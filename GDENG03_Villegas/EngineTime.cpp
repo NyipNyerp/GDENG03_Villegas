259e0f919cc8a9f0e895690f2a6c21415ae1a527
Meep <79883211+NyipNyerp@users.noreply.github.com>
2023-09-25 22:48:04 +0800

    Quick edit

--- GDENG03_Villegas/EngineTime.cpp
+++ GDENG03_Villegas/EngineTime.cpp
@@ -1,33 +1,33 @@
-#include "EngineTime.h"
-
-EngineTime::EngineTime()
-{
-}
-
-EngineTime::~EngineTime()
-{
-}
-
-void EngineTime::initialize()
-{
-    if (!sharedInstance)
-    {
-        sharedInstance = new EngineTime();
-    }
-
-}
-
-double EngineTime::getDeltaTime()
-{
-    return 0.0;
-}
-
-void EngineTime::logFrameStart()
-{
-    sharedInstance->start = std::chrono::system_clock::now();
-}
-
-void EngineTime::logFrameEnd()
-{
-    sharedInstance->end = std::chrono::system_clock::now();
-}
+//#include "EngineTime.h"
+//
+//EngineTime::EngineTime()
+//{
+//}
+//
+//EngineTime::~EngineTime()
+//{
+//}
+//
+//void EngineTime::initialize()
+//{
+//    if (!sharedInstance)
+//    {
+//        sharedInstance = new EngineTime();
+//    }
+//
+//}
+//
+//double EngineTime::getDeltaTime()
+//{
+//    return 0.0;
+//}
+//
+//void EngineTime::logFrameStart()
+//{
+//    sharedInstance->start = std::chrono::system_clock::now();
+//}
+//
+//void EngineTime::logFrameEnd()
+//{
+//    sharedInstance->end = std::chrono::system_clock::now();
+//}
